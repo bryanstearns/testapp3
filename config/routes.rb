8c4bc8f77b1b185beefffa0538524dfd36755c6a
Bryan Stearns <bryanstearns@gmail.com>
2012-02-27 17:29:29 -0800

    add things scaffold; replace root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Testapp3::Application.routes.draw do
+  resources :things
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Testapp3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'things#index'
 
   # See how all your routes lay out with "rake routes"
 
